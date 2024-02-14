a213f21741b924bca78cadba5ae1bb1f6f819ed4
zitons <zitons@outlook.com>
2024-02-14 16:41:16 +0800

    试验站点地图

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,13 +1,14 @@
 /*
  * @Author: zitons
  * @Date: 2024-02-13 12:38:36
- * @LastEditors: 
- * @LastEditTime: 2024-02-13 12:39:08
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2024-02-14 16:40:55
  * @Description: 简介
  */
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: process.env.BLOG,
     generateRobotsTxt: true, // (optional)
+    outDir :'public/xml'
     // ...other options
-  }
+}
